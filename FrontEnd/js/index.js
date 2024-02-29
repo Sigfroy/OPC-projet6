0d1239d21b7dd87a02d62f56a0ce04e91cb16d25
Sony Ah-Sam <sony.ahsam1@gmail.com>
2024-02-29 14:45:41 +0100

    feat: implement API request handling for categories

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -3,6 +3,11 @@
  */
 const apiWorks = fetch('http://localhost:5678/api/works');
 const categories = fetch('http://localhost:5678/api/categories');
+const gallery = document.querySelector('.gallery');
+const portfolio = document.getElementById('portfolio');
+const buttonFiltre = document.querySelector('.button');
+
+let allWorks = []; // Stocke la liste complète des projets récupérée depuis l'API.
 
 /*
  * Traite la réponse de la requête vers l'API des projets.
@@ -19,5 +24,63 @@ apiWorks.then(async (responseApiWorks) => {
         displayWorks(allWorks);
     } else {
         console.error("La galerie n'a pas été trouvée.");
+    }
+});
+
+/*
+ * Traite la réponse de la requête vers l'API des catégories.
+ * Crée les boutons de filtres et la classe active.
+ */
+categories.then(async (responseCategories) => {
+    if (!responseCategories.ok) {
+        throw new Error('Erreur lors de la récupération des catégories');
+    }
+
+    const dataCategories = await responseCategories.json();
+
+    if (portfolio) {
+        const filtres = document.createElement('div');
+        filtres.classList.add('filtres');
+
+        const tousButton = document.createElement('button');
+        tousButton.textContent = 'Tous';
+        tousButton.classList.add('button', 'active');
+        tousButton.addEventListener('click', () => {
+            document
+                .querySelectorAll('.button')
+                .forEach((btn) => btn.classList.remove('active'));
+
+            tousButton.classList.add('active');
+            displayWorks(allWorks);
+        });
+
+        filtres.appendChild(tousButton);
+
+        dataCategories.forEach((category) => {
+            const buttonElement = document.createElement('button');
+            buttonElement.classList.add('button');
+            buttonElement.textContent = category.name;
+
+            buttonElement.addEventListener('click', () => {
+                document
+                    .querySelectorAll('.button')
+                    .forEach((btn) => btn.classList.remove('active'));
+
+                buttonElement.classList.add('active');
+                const filteredWorks = filterWorksByCategory(category);
+                displayWorks(filteredWorks);
+            });
+
+            filtres.appendChild(buttonElement);
+        });
+
+        portfolio.appendChild(filtres);
+        portfolio.insertBefore(filtres, gallery);
+
+        if (JSON.parse(sessionStorage.getItem('connected'))) {
+            filtres.style.display = 'none';
+        }
+    } else {
+        console.error("Les catégories n'ont pas été trouvées.");
     }
 });
