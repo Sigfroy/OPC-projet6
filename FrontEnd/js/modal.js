b1a95ce129cf9d465b5d01bb0d20f8ed35bc0a88
Sony Ah-Sam <sony.ahsam1@gmail.com>
2024-02-29 15:43:16 +0100

    featimplement modal to add new project

--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -47,3 +47,59 @@ apiWorksModal.then(async (responseApiWorksModal) => {
         console.error("La galerie n'a pas été trouvée.");
     }
 });
+
+// GESTION D'AFFICHAGE DES MODALES
+const modalContainer = document.getElementById('modal-container');
+const buttonModify = document.getElementById('modify');
+const modalPhoto = document.getElementById('modal-photo');
+
+// Ajoute un écouteur au bouton de modification pour afficher la modale principale et masquer la modale d'ajout de projet.
+buttonModify.addEventListener('click', () => {
+    modalContainer.style.display = 'block';
+    modalPhoto.style.display = 'none';
+    modal.style.display = 'flex';
+});
+
+// FERMETURE ET OUVERTURE MODALES
+const closeModal = document.getElementById('modal-close');
+const overlay = document.querySelector('.overlay');
+const addPhoto = document.getElementById('add-photo');
+const modal = document.getElementById('modal');
+const closeModalPhoto = document.getElementById('modal-photo-close');
+const modalReturn = document.getElementById('modal-return');
+
+// Ajoute un écouteur au bouton de fermeture de la modale principale pour masquer la modale.
+closeModal.addEventListener('click', () => {
+    modalContainer.style.display = 'none';
+});
+
+// Ajoute un écouteur à l'overlay pour masquer les modales.
+overlay.addEventListener('click', () => {
+    modalContainer.style.display = 'none';
+    resetImage();
+    title.value = '';
+    category.value = '';
+});
+
+// Ajoute un écouteur au bouton d'ajout de photo pour masquer la modale principale et afficher la modale d'ajout de projet.
+addPhoto.addEventListener('click', () => {
+    modal.style.display = 'none';
+    modalPhoto.style.display = 'flex';
+});
+
+// Ajoute un écouteur au bouton de fermeture de la modale d'ajout de projet pour masquer les modales.
+closeModalPhoto.addEventListener('click', () => {
+    modalContainer.style.display = 'none';
+    resetImage();
+    title.value = '';
+    category.value = '';
+});
+
+// Ajoute un écouteur au bouton de retour à la modale principale depuis la modale d'ajout de projet pour masquer la modale d'ajout de projet et afficher la modale principale.
+modalReturn.addEventListener('click', () => {
+    modalPhoto.style.display = 'none';
+    modal.style.display = 'flex';
+    resetImage();
+    title.value = '';
+    category.value = '';
+});
